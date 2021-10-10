1f99b6cb5c3ef4bb41b644a4b583aea5ad9a42ad
bluxa <81200014+bluxa@users.noreply.github.com>
2021-10-10 11:59:18 -0600

    Lista de procesos

--- ProyectoSimuladorProceso/Form1.Designer.cs
+++ ProyectoSimuladorProceso/Form1.Designer.cs
@@ -28,13 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCpu = new System.Windows.Forms.TextBox();
+            this.txtMemoria = new System.Windows.Forms.TextBox();
+            this.txtQuantum = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipoProceso = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picMinimizar = new System.Windows.Forms.PictureBox();
+            this.picRestaurar = new System.Windows.Forms.PictureBox();
+            this.picCerrar = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRestaurar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCpu);
+            this.groupBox1.Controls.Add(this.txtMemoria);
+            this.groupBox1.Controls.Add(this.txtQuantum);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbTipoProceso);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(30, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(245, 318);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Información del Proceso";
+            // 
+            // txtCpu
+            // 
+            this.txtCpu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCpu.Location = new System.Drawing.Point(29, 266);
+            this.txtCpu.Name = "txtCpu";
+            this.txtCpu.ReadOnly = true;
+            this.txtCpu.Size = new System.Drawing.Size(184, 26);
+            this.txtCpu.TabIndex = 7;
+            // 
+            // txtMemoria
+            // 
+            this.txtMemoria.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMemoria.Location = new System.Drawing.Point(29, 197);
+            this.txtMemoria.Name = "txtMemoria";
+            this.txtMemoria.ReadOnly = true;
+            this.txtMemoria.Size = new System.Drawing.Size(184, 26);
+            this.txtMemoria.TabIndex = 6;
+            // 
+            // txtQuantum
+            // 
+            this.txtQuantum.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantum.Location = new System.Drawing.Point(29, 129);
+            this.txtQuantum.Name = "txtQuantum";
+            this.txtQuantum.ReadOnly = true;
+            this.txtQuantum.Size = new System.Drawing.Size(184, 26);
+            this.txtQuantum.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Uso del CPU";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Memoria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de proceso";
+            // 
+            // cmbTipoProceso
+            // 
+            this.cmbTipoProceso.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipoProceso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbTipoProceso.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTipoProceso.FormattingEnabled = true;
+            this.cmbTipoProceso.Items.AddRange(new object[] {
+            "Impresión",
+            "Música",
+            "Mozilla",
+            "Netbeans",
+            "Excel",
+            "Base de Datos"});
+            this.cmbTipoProceso.Location = new System.Drawing.Point(29, 60);
+            this.cmbTipoProceso.Name = "cmbTipoProceso";
+            this.cmbTipoProceso.Size = new System.Drawing.Size(184, 27);
+            this.cmbTipoProceso.TabIndex = 0;
+            this.cmbTipoProceso.SelectedIndexChanged += new System.EventHandler(this.cmbTipoProceso_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.picMinimizar);
+            this.panel1.Controls.Add(this.picRestaurar);
+            this.panel1.Controls.Add(this.picCerrar);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1368, 51);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(405, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(500, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SIMULADOR CICLO DE VIDA DE LOS PROCESOS";
+            // 
+            // picMinimizar
+            // 
+            this.picMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("picMinimizar.Image")));
+            this.picMinimizar.Location = new System.Drawing.Point(1238, 10);
+            this.picMinimizar.Name = "picMinimizar";
+            this.picMinimizar.Size = new System.Drawing.Size(24, 24);
+            this.picMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picMinimizar.TabIndex = 2;
+            this.picMinimizar.TabStop = false;
+            this.picMinimizar.Click += new System.EventHandler(this.picMinimizar_Click);
+            // 
+            // picRestaurar
+            // 
+            this.picRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("picRestaurar.Image")));
+            this.picRestaurar.Location = new System.Drawing.Point(1281, 10);
+            this.picRestaurar.Name = "picRestaurar";
+            this.picRestaurar.Size = new System.Drawing.Size(24, 24);
+            this.picRestaurar.TabIndex = 2;
+            this.picRestaurar.TabStop = false;
+            // 
+            // picCerrar
+            // 
+            this.picCerrar.Image = ((System.Drawing.Image)(resources.GetObject("picCerrar.Image")));
+            this.picCerrar.Location = new System.Drawing.Point(1324, 10);
+            this.picCerrar.Name = "picCerrar";
+            this.picCerrar.Size = new System.Drawing.Size(24, 24);
+            this.picCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCerrar.TabIndex = 2;
+            this.picCerrar.TabStop = false;
+            this.picCerrar.Click += new System.EventHandler(this.picCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 439);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simulador";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRestaurar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCerrar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picCerrar;
+        private System.Windows.Forms.PictureBox picRestaurar;
+        private System.Windows.Forms.PictureBox picMinimizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTipoProceso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQuantum;
+        private System.Windows.Forms.TextBox txtCpu;
+        private System.Windows.Forms.TextBox txtMemoria;
     }
 }
 
--- ProyectoSimuladorProceso/Form1.cs
+++ ProyectoSimuladorProceso/Form1.cs
@@ -16,5 +16,51 @@ namespace ProyectoSimuladorProceso
         {
             InitializeComponent();
         }
+
+        private void picCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void picMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void cmbTipoProceso_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = cmbTipoProceso.SelectedIndex;
+
+            switch (indice)
+            {
+                case 0:
+                    txtQuantum.Text = "1 minuto";
+                    txtMemoria.Text = "500 Kb";
+                    txtCpu.Text = "10%";
+                    break;
+                case 1:
+                    txtQuantum.Text = "45 segundos";
+                    txtMemoria.Text = "10 mb";
+                    txtCpu.Text = "20%";
+                    break;
+                case 2:
+                    txtQuantum.Text = "30 segundos";
+                    txtMemoria.Text = "50 mb";
+                    txtCpu.Text = "30%";
+                    break;
+                case 3:
+                    txtQuantum.Text = "2 minutos";
+                    txtMemoria.Text = "1 gb";
+                    txtCpu.Text = "70%";
+                    break;
+                case 4:
+                    txtQuantum.Text = "30 segundos";
+                    txtMemoria.Text = "256 mb";
+                    txtCpu.Text = "50%";
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
